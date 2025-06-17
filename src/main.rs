bf76aef056786f2abe1e979c4a2c164dda3aa628
luedu1103 <luedu1103@gmail.com>
2025-06-17 16:18:20 -0500

    nur test: fix url

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn print_shit(input: &str) {
  if input.is_empty() {
         println!("No input provided.");
     } else {
-        println!("Hello from Nur!!! Pls work properly!: {}", input);
+        println!("Hello from Nur!!! Problem with the url!: {}", input);
     }
 }
 
